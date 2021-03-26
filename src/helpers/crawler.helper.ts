bd8c6dad63ce0d4f57da22ea29e428e405e44b2a
Kaltu <lkaxtu@gmail.com>
2021-03-26 18:29:05 +0800

    [What] Dirty commit

--- src/helpers/crawler.helper.ts
+++ src/helpers/crawler.helper.ts
@@ -25,25 +25,46 @@ export default async function getTwitterImageUrls(url: string): Promise<string[]
       waitUntil: 'networkidle2'
     });
   } catch (err) {
-    throw err;
+    return [];
   }
 
-  let retry = 3;
-  const pbs_img_urls: string[] = [];
+  try {
+    const showNSFW_selector = "body div[data-focusable='true'] > div[dir='auto'] > span > span";
+    page.waitForSelector(showNSFW_selector, { visible: true, timeout: 10000 });
+    page.click(showNSFW_selector);
+  } catch (err) {
+    ;  // it's okay
+  }
+
+  const statusId = url.replace(/^.*\//, '');
+  // const xpath = `//body//a[contains(@href, '${statusId}')]/ancestor::div[position()=1]//img`;
+  const css_selector = `video[src], a[href*="${statusId}"] img[src]`;
+  page.waitForSelector(css_selector, { visible: true, timeout: 30000 });
+
+  let retry = 10;
+  const valid_urls: string[] = [];
   while (retry--) {
     // Crawl src for all <img> tags
-    const img_urls = await page.$$eval('img[src]', imgs => imgs.map(img => img.getAttribute('src')));
+    const media_srcs = await page.$$eval(css_selector, imgs => imgs.map(img => img.getAttribute('src')));
+    if (!media_srcs.length) {
+      await wait(1000);
+      if (debug) {
+        console.log(`retry: ${10 - retry}`, media_srcs);
+      }
+      continue;
+    }
 
-    for (const img_url of img_urls) {
-      if (img_url?.includes('pbs.twimg.com/media')) {
-        pbs_img_urls.push(img_url.replace(/&name=.+$/, '&name=large'));
+    for (const src of media_srcs) {
+      if (src?.includes('pbs.twimg.com/media') ||
+        src?.includes('video.twimg.com/tweet_video')) {
+        valid_urls.push(src.replace(/&name=.+$/, '&name=large'));
       }
     }
     if (debug) {
-      console.log(`retry: ${3 - retry}`, pbs_img_urls);
+      console.log(`retry: ${10 - retry}`, valid_urls);
     }
 
-    if (pbs_img_urls.length) {
+    if (valid_urls.length) {
       break;
     }
 
@@ -53,5 +74,22 @@ export default async function getTwitterImageUrls(url: string): Promise<string[]
 
   await browser.close();
 
-  return pbs_img_urls;
+  return valid_urls;
 };
+
+if (process.env.NODE_ENV !== 'production') {
+  process.on('uncaughtException', exception => console.log(exception));
+
+  const video_url = 'https://twitter.com/trinityfate62/status/1375130608049954820';
+  // getTwitterImageUrls(video_url);
+
+  const multi_img_url = 'https://twitter.com/sora1013siba/status/1374930115574996996';
+  (async () => {
+    try { await getTwitterImageUrls(multi_img_url); }
+    catch (err) { console.log(err); }
+  })();
+
+  const img_url = 'https://twitter.com/Machiccalition/status/1374828782620565504';
+  // getTwitterImageUrls(img_url);
+
+}
