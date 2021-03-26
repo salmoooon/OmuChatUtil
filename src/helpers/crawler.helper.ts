2ea0b0e32bf9c48fd12f62d5d1d4555265169e4f
Kaltu <lkaxtu@gmail.com>
2021-03-26 20:15:27 +0800

    [What][WIP] first attempt

--- src/helpers/crawler.helper.ts
+++ src/helpers/crawler.helper.ts
@@ -1,3 +1,10 @@
+// puppeteer-extra is a drop-in replacement for puppeteer,
+// it augments the installed puppeteer with plugin functionality
+// import puppeteer from 'puppeteer-extra';
+
+// add stealth plugin and use defaults (all evasion techniques)
+// import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+// puppeteer.use(StealthPlugin());
 import puppeteer from 'puppeteer';
 
 function wait(ms: number) {
@@ -6,12 +13,24 @@ function wait(ms: number) {
 
 export default async function getTwitterImageUrls(url: string): Promise<string[]> {
   const debug = process.env.NODE_ENV !== 'production';
-  let browser = await puppeteer.launch({
+  // Viewport && Window size
+  const width = 1280;
+  const height = 720;
+
+  const browser = await puppeteer.launch({
     headless: !debug,
     slowMo: 100,
+    args: [
+      `--window-size=${width},${height}`
+    ],
+    defaultViewport: {
+      width,
+      height
+    }
   });
 
-  let page = await browser.newPage();
+  const page = await browser.newPage();
+  await page.setBypassCSP(true);
 
   try {
     await page.goto(url, {
@@ -19,29 +38,43 @@ export default async function getTwitterImageUrls(url: string): Promise<string[]
       waitUntil: 'networkidle2'
     });
   } catch (err) {
+    await browser.close();
     return [];
   }
 
   const statusId = url.replace(/^.*\//, '');
   // const xpath = `//body//a[contains(@href, '${statusId}')]/ancestor::div[position()=1]//img`;
-  const css_selector = `a[href*="/${statusId}/"] img[src]`;
-  page.waitForSelector(css_selector, { visible: true, timeout: 30000 });
+  const css_selector = `a[href*="/${statusId}/"] video[src], a[href*="/${statusId}/"] img[src]`;
+  try {
+    await page.waitForSelector(css_selector, { visible: true, timeout: 10000 });
+  } catch (err) {
+    // await browser.close();
+    return []; // target tags not found
+  }
+
+  if (debug) {
+    console.log('target element tag found');
+  }
 
   let retry = 10;
   const valid_urls: string[] = [];
   while (retry--) {
     // Crawl src for all <img> tags
-    const media_srcs = await page.$$eval(css_selector, imgs => imgs.map(img => img.getAttribute('src')));
+    let media_srcs: any[] = [];
+    try {
+      media_srcs = await page.$$eval(css_selector, imgs => imgs.map(img => img.getAttribute('src')));
+    } catch (err) { }
     if (!media_srcs.length) {
       await wait(1000);
       if (debug) {
-        console.log(`retry: ${10 - retry}`, media_srcs);
+        console.log(`retry: ${10 - retry} []`);
       }
       continue;
     }
 
     for (const src of media_srcs) {
-      if (src?.includes('pbs.twimg.com/media')) {
+      if (src?.includes('pbs.twimg.com/media') ||
+        src?.includes('pbs.twimg.com/tweet_video_thumb/')) {
         valid_urls.push(src.replace(/&name=.+$/, '&name=large'));
       }
     }
@@ -66,16 +99,16 @@ export default async function getTwitterImageUrls(url: string): Promise<string[]
 if (process.env.NODE_ENV !== 'production') {
   process.on('uncaughtException', exception => console.log(exception));
 
+  const gif_url = 'https://twitter.com/etranger_anime/status/1366765246950555652';
+  // getTwitterImageUrls(gif_url);
+
   const video_url = 'https://twitter.com/trinityfate62/status/1375130608049954820';
-  // getTwitterImageUrls(video_url);
+  getTwitterImageUrls(video_url);
 
   const multi_img_url = 'https://twitter.com/sora1013siba/status/1374930115574996996';
-  (async () => {
-    try { await getTwitterImageUrls(multi_img_url); }
-    catch (err) { console.log(err); }
-  })();
+  // getTwitterImageUrls(multi_img_url);
 
   const img_url = 'https://twitter.com/Machiccalition/status/1374828782620565504';
-  getTwitterImageUrls(img_url);
+  // getTwitterImageUrls(img_url);
 
 }
